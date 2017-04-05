ad103a0ab619d7ef7eb0bbd22391ec08fd74d2b6
antoniotrkdz <antoniotrkdz@gmail.com>
2017-04-05 16:36:04 +0100

    Add database build file and script relates #7

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # week7-sweetpotato-cms
+
+Database schema:
+
+### Users: 1 to 1
+Column | Type | Modifiers
+--- | --- | ---
+user_id | integer | not null default
+username | character varying(100) | not null
+password | character varying(50) | not null
+isAdmin | boolean | not null
+
+### Posts: 1 to Many
+
+Column | Type | Modifiers
+--- | --- | ---
+post_id | integer | not null default
+title | character varying(50) | not null
+body | character varying(500) | not null
+date | date | not null
+user_id | integer | not null
---
+++ database/db_build.sql
@@ -0,0 +1,33 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users, posts;
+
+CREATE TABLE users (
+  user_id          serial        PRIMARY KEY,
+  username        varchar(100)  NOT NULL,
+  password        varchar(100)  NOT NULL,
+  isAdmin         BOOLEAN       NOT NULL
+);
+
+CREATE TABLE posts (
+  post_id              serial          PRIMARY KEY,
+  title           varchar(50)     NOT NULL,
+  body            varchar(500)    NOT NULL,
+  date            date            NOT NULL,
+  user_id         integer         REFERENCES users(user_id)
+);
+
+-- 1 TO MANY
+INSERT INTO users(username, password, isAdmin) VALUES
+('antonio', 'pwd1', TRUE),
+('martha', 'pwd2', TRUE),
+('yvonne','pwd3', TRUE),
+('zooey','pwd4', TRUE);
+-- 1 TO 1
+INSERT INTO posts(title, body, date, user_id) VALUES
+('Lorem1', '111-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam magni maxime dicta ullam aut, sunt. Quidem veniam unde, minima, velit tempore odit voluptas alias, harum tenetur placeat animi, quaerat perspiciatis?','2017-04-01', 1),
+('Lorem2', '222-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam magni maxime dicta ullam aut, sunt. Quidem veniam unde, minima, velit tempore odit voluptas alias, harum tenetur placeat animi, quaerat perspiciatis?', '2017-04-02', 2),
+('Lorem3', '333-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam magni maxime dicta ullam aut, sunt. Quidem veniam unde, minima, velit tempore odit voluptas alias, harum tenetur placeat animi, quaerat perspiciatis?', '2017-04-03', 3),
+('Lorem4', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam magni maxime dicta ullam aut, sunt. Quidem veniam unde, minima, velit tempore odit voluptas alias, harum tenetur placeat animi, quaerat perspiciatis?','2017-04-04', 4);
+
+COMMIT;
