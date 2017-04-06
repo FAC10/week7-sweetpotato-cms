7c0330c6193157aa439ed8f521da9d9cca0887fe
Yvonne Liu <liuyvonne.ca@gmail.com>
2017-04-06 16:55:05 +0100

    fix hapi-auth-basic to password form
    relates to #22

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,6 @@
+const dbConnection = require('../database/db_connection.js');
+const bcrypt = require('bcrypt');
+
 const home = {
   method: 'GET',
   path: '/',
@@ -6,18 +9,47 @@ const home = {
   },
 };
 
-const createPost = {
+const loginPage = {
   method: 'GET',
-  path: '/create-post',
-  config: {
-    auth: 'simple',
-    handler: (req, reply) => {
-      console.log(req.auth.credentials, '<<<<<<<');
-      reply.view('create-post', {
-        credentials: req.auth.credentials,
-      });
-    },
+  path: '/login-page',
+  handler: (req, reply) => {
+    reply.view('login-page');
   },
+};
+
+const login = {
+  method: 'POST',
+  path: '/login',
+  handler: (req, reply) => {
+    const username = req.payload.username;
+    const password = req.payload.password;
+
+    dbConnection.query(`SELECT * from users WHERE users.username = '${username}';`, (err, res) => {
+      if (err) throw err;
+      const user = res.rows[0];
+      bcrypt.compare(password, user.password, (err, isValid) => {
+        if (err) throw err;
+        if (isValid) {
+          reply.view('create-post', {
+            credentials: { name: username },
+          });
+        } else {
+          reply.view('failed-login');
+        }
+      });
+    });
+  },
+};
+
+// const createPost = {
+//   method: 'GET',
+//   path: '/create-post',
+//   handler: (req, reply) => {
+//     reply.view('create-post', {
+//       credentials: req.auth.credentials,
+//     });
+//   },
+// };
   // handler: (req, reply) => {
   //   const username = req.payload.username;
   //   const password = req.payload.password;
@@ -27,7 +59,6 @@ const createPost = {
   //     credentials: req.auth.credentials,
   //   });
   // },
-};
 
 const authRoute = {
   method: 'GET',
@@ -39,6 +70,7 @@ const authRoute = {
 
 module.exports = [
   home,
-  createPost,
+  loginPage,
+  login,
   authRoute,
 ];
