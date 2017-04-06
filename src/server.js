cb723e96c9759daa56931ffbca1e298b2e8b38c1
antoniotrkdz <antoniotrkdz@gmail.com>
2017-04-06 14:44:37 +0100

    Add cookie session management relates #25

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const inert = require('inert');
 const routes = require('./routes');
 const vision = require('vision');
 const Handlebars = require('handlebars');
+const cookieAuth = require('hapi-auth-cookie');
 
 const server = new hapi.Server();
 
@@ -10,13 +11,22 @@ server.connection({
   port: process.env.PORT || 4000,
 });
 
-server.register([inert, vision], (err) => {
+server.register([inert, vision, cookieAuth], (err) => {
   if (err) throw err;
 
   server.views({
     engines: { hbs: Handlebars },
     path: './src/views',
   });
+
+  const options = {
+    password: 'pwd1',
+    cookie: 'cookie-name',
+    isSecure: false,
+    ttl: 24 * 60 * 60 * 1000,
+  };
+
+  server.auth.strategy('base', 'cookie', 'optional', options);
 
   server.route(routes);
 });
