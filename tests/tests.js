0453a2f3f2c863fd629a51d19bb865288dc6b30e
Yvonne Liu <liuyvonne.ca@gmail.com>
2017-04-07 11:49:48 +0100

    add basic tests to router
    relates to #42

--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,30 @@
-const test = require('tape');
+const tape = require('tape');
+const server = require('../src/server.js');
 
-test('Check tape is working with a simple passing test', (t) => {
+tape('Check tape is working with a simple passing test', (t) => {
   t.pass('a message to print out on sucess');
   t.end();
+});
+
+tape('check if invalid route returns 404', (t) => {
+  const options = {
+    url: '/blah',
+    method: 'GET',
+  };
+  server.inject(options, (res) => {
+    t.equal(res.statusCode, 404, 'Invalid route should return 404');
+    t.end();
+  });
+});
+
+
+tape('Check if home route returns 200', (t) => {
+  const options = {
+    url: '/',
+    method: 'GET',
+  };
+  server.inject(options, (res) => {
+    t.equal(res.statusCode, 200, 'correct home route should return 200');
+    t.end();
+  });
 });
